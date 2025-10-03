54b41e10f87dcb19ee743058fcaf1ba857449edc
macnotes <25835285+macnotes@users.noreply.github.com>
2025-10-02 23:59:03 -0400

    [OSS] These secrets were fake, but let's make sure they're explicitly so.

--- examples/resources/jsc_uemc/resource.tf
+++ examples/resources/jsc_uemc/resource.tf
@@ -1,5 +1,5 @@
 resource "jsc_uemc" "my_uemc_config" {
-  domain       = "https://terraform.jamfcloud.com/"
-  clientid     = "1b752ccb-eaee-4250-a202-a5d1d091053c"
-  clientsecret = "bvaKmX7voLbvk7uEWm9ET3-dfdfd-rPjpVxAjhniNNBCHRKSdx9EvRGKZmHp66jB"
+  domain       = "https://your.jamfcloud.com/"
+  clientid     = "aaaaaaaa-aaaa-aaaa-a999-a9a9a999a99a"
+  clientsecret = "aaaAaA9aaAaaa9aAAa9AA9-aaaaa-aAaaaAaAaaaAAAAAAaaaAaAAAAaaaAa99aA"
 }
